e515219a99c2715b18c0f820ec15cfaf84f83283
Joshua Zenn <wordman05@gmail.com>
2015-06-10 20:30:34 -0400

    Update index2.php
    Fix for missing absolute path for PHP source code.

--- index2.php
+++ index2.php
@@ -66,7 +66,7 @@
     <div class="row">
       <div class="col-sm-12">
         <hr />
-        <?php require_once './src/php/version.php'; ?>
+        <?php require_once 'https://enigmatic-thicket-1799.herokuapp.com/src/php/version.php'; ?>
         <p>Copyright 2015 Otter Nonsense Interactive - CI build: <?php echo $version; ?></p>
       </div>
     </div>
