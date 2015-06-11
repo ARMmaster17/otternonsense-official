7cf41b52d45d395d48b0c438d486a263a4f98391
Joshua Zenn <jzenn@students.kennesaw.edu>
2015-06-11 18:19:44 +0000

    Sinatra app with same interface as existing PHP code

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'sinatra'
+require 'slim'
 
 set :port, ENV['PORT']
 set :bind, ENV['IP']
 
 get '/' do
-  'Hello, world!'
+  slim :index
+end
+get '/superkey' do
+    slim :index2
 end
