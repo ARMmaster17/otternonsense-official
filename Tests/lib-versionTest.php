1f0693321b26aed9bc73077f50c2588fe56a7ff1
Joshua Zenn <wordman05@gmail.com>
2015-06-11 10:36:15 -0400

    Update lib-versionTest.php

--- Tests/lib-versionTest.php
+++ Tests/lib-versionTest.php
@@ -1,11 +1,11 @@
 <?php
-class lib_version extends PHPUnit_Framework_TestCase
+class lib-version extends PHPUnit_Framework_TestCase
 {
     public function testgetVersion()
     {
         //$this->assertNotEmpty(getVersion());
         // Placeholder until we get unit testing working
-        $this->assertEqual(1, 1);
+        //$this->assertEqual(1, 1);
     }
 }
 ?>
