58ceac5abc4aa18d82b222065c8d19faf107b2f9
Joshua Zenn <wordman05@gmail.com>
2015-06-11 16:20:52 -0400

    Delete test.sh

--- build/test/test.sh
+++
@@ -1,7 +0,0 @@
-echo 'WARNING: Testing not configured!'
-echo 'WARNING: No unit tests found!'
-echo 'WARNING: Sauce credentials not found!'
-echo 'WARNING: Sauce config files not found!'
-echo 'Using phpunit...'
-phpunit
-echo 'Testing complete, returning to Travis CI build stack...'
