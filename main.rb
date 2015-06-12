41250649987c6f836d14181167631cbed15ac0ba
Joshua Zenn <wordman05@gmail.com>
2015-06-12 11:42:11 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,16 +6,18 @@ require 'slim'
 set :port, ENV['PORT'] || 8080
 set :bind, ENV['IP'] || '0.0.0.0'
 
-BUILD = 'Error'
-f = File.open("./builddata.txt", "r")
-BUILD = f[0]
-f.close
+#BUILD = 'Error'
+#f = File.open("./builddata.txt", "r")
+#BUILD = f[0]
+#f.close
 
 get '/' do
-  @TRAVISBUILDNUMBER = BUILD
+  #@TRAVISBUILDNUMBER = BUILD
+  @TRAVISBUILDNUMBER = 'ERROR'
   slim :index
 end
 get '/superkey' do
-  @TRAVISBUILDNUMBER = BUILD
+  #@TRAVISBUILDNUMBER = BUILD
+  @TRAVISBUILDNUMBER = 'ERROR'
   slim :index2
 end
