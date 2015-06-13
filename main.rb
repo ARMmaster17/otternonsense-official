8e742b8336b5cbd6e9335870a29123bcaf38f37c
Joshua Zenn <wordman05@gmail.com>
2015-06-13 10:58:41 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -6,6 +6,12 @@ require 'slim'
 set :port, ENV['PORT'] || 8080
 set :bind, ENV['IP'] || '0.0.0.0'
 
+helpers do
+  def partial(template, locals = {})
+    slim template, :layout => false, :locals => locals
+  end
+end
+
 #BUILD = 'Error'
 #f = File.open("./builddata.txt", "r")
 #BUILD = f[0]
